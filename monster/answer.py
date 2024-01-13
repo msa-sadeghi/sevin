c327853489139e2c158b6e5acf641b1fcdb4e081
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-13 20:50:17 +0330

    quiz
    quiz

---
+++ monster/answer.py
@@ -0,0 +1,33 @@
+age = int(input("enter your age: "))
+if age < 7:
+    print("Kid")
+elif age>=7 and age <=13:
+    print("teenager")
+elif age > 13 and age < 18:
+    print("junior")
+elif age >= 18:
+    print("adult")
+
+
+
+# else:
+#     if age>=7 and age <=13:
+#         print("teenager")
+#     else:
+#         if age > 13 and age < 18:
+#             print("junior")
+#         else:
+#             if age >= 18:
+#                 print("adult")
+
+
+
+
+
+
+
+# if age >= 7:
+#     print("teenager")
+
+# if age >= 18:
+#     print("Adult")
