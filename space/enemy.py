11dadb55589b331b8414f1cd5f02786264bef699
msa-sadeghi <sadeghi.msai@gmail.com>
2024-02-10 20:59:44 +0330

    s

--- space/enemy.py
+++ space/enemy.py
@@ -1,1 +1,20 @@
-# TODO enemy class
+from pygame.sprite import Sprite
+import pygame
+class Enemy(Sprite):
+    def __init__(self, x,y, group):
+        super().__init__()
+        img= pygame.image.load("assets/chick.png")
+        img_w = img.get_width()
+        img_h = img.get_height()
+        self.image = pygame.transform.scale(img, (img_w * 1, img_h * 1))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        group.add(self)
+        self.direction = 1
+        self.speed = 3
+        
+    def update(self):
+        self.rect.x += self.speed * self.direction
+        
+        
+    
--- space/main.py
+++ space/main.py
@@ -1,8 +1,31 @@
 from constants import *
 from player import Player
+from enemy import Enemy
 
+level = 0
+
+def check_on_edge():
+    on_edge = False
+    for enemy in enemy_group:
+        if enemy.rect.left < 0 or enemy.rect.right > SCREEN_WIDTH:
+            on_edge = True
+            break
+    if on_edge:
+        for enemy in enemy_group:
+            enemy.direction *= -1
+            enemy.rect.y += level *10
+
+
+def spawn_enemies():
+    global level
+    level += 1
+    for i in range(4):
+        for j in range(10):
+            Enemy(j * 96, i * 96, enemy_group)
+        
 my_player = Player()
-
+enemy_group = pygame.sprite.Group()
+spawn_enemies()
 running = True
 while running:
     for event in pygame.event.get():
@@ -10,7 +33,10 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
     screen.fill((0,0,0)) 
+    check_on_edge()
     my_player.move()
-    my_player.draw()         
+    my_player.draw()  
+    enemy_group.update()       
+    enemy_group.draw(screen)
     pygame.display.update()
     CLOCK.tick(FPS)
