c8c20dfca4f0e019fefb619cd049a7a9e7cc9209
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-07-04 21:00:01 +0330

    codes

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,25 @@
+// function getPrice() {
+//   console.log("start");
+//   console.log("end");
+// }
+// getPrice();
+
+// function delay() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve("data recieved"), 2000);
+//   });
+// }
+
+// async function getPrice() {
+//   console.log("start");
+//   const result = await delay();
+//   console.log(result);
+//   console.log("end");
+// }
+
+const rootElement = document.getElementById("root");
+
+getPrice();
 async function getPrice() {
   const url =
     "https://brsapi.ir/Api/Tsetmc/Sample/Api_FreeBourseWebService.json";
@@ -7,7 +29,17 @@ async function getPrice() {
     console.log("error");
   }
   const data = await response.json();
-  console.log(data);
+  for (let i = 0; i < data.length; i++) {
+    const cardElement = document.createElement("div");
+    const h3 = document.createElement("h3");
+    h3.classList.add("title");
+    h3.innerText = data[i]["l30"];
+    cardElement.append(h3);
+
+    console.log(data[i]);
+
+    rootElement.append(cardElement);
+  }
 }
 
 getPrice();
