3ea118b9dc041a3b8e21ca7c25ef5ace5e6f024f
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-07-18 20:19:38 +0330

    codes

--- javascript/script.js
+++ javascript/script.js
@@ -17,8 +17,20 @@
 //   console.log("end");
 // }
 
-const rootElement = document.getElementById("root");
+function getPageData() {
+  for (let i = 0; i < SizeCount; i++) {
+    const cardElement = document.createElement("div");
+    const h3 = document.createElement("h3");
+    h3.classList.add("title");
+    h3.innerText = data[i]["l30"];
+    cardElement.append(h3);
+    rootElement.append(cardElement);
+  }
+}
 
+const rootElement = document.getElementById("root");
+const SizeCount = 10;
+let pageCount;
 async function getPrice() {
   const url =
     "https://brsapi.ir/Api/Tsetmc/Sample/Api_FreeBourseWebService.json";
@@ -34,14 +46,10 @@ async function getPrice() {
       rootElement.append(loadingSpan);
     }
     const data = await response.json();
-    for (let i = 0; i < data.length; i++) {
-      const cardElement = document.createElement("div");
-      const h3 = document.createElement("h3");
-      h3.classList.add("title");
-      h3.innerText = data[i]["l30"];
-      cardElement.append(h3);
-      rootElement.append(cardElement);
-    }
+    pageCount = Math.floor(data.length / SizeCount);
+    console.log(pageCount);
+    getPageData();
+
     rootElement.removeChild(loadingDiv);
   } catch (error) {
     loadingSpan.innerText = "خطای سرور   ...";
