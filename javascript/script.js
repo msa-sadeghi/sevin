e45c5a08623fa6cce1ec020b342aeae5e6e0b8ce
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-07-18 20:59:38 +0330

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -17,18 +17,22 @@
 //   console.log("end");
 // }
 
-function getPageData(data) {
-  for (let i = 0; i < SizeCount; i++) {
+function getPageData() {
+    container.innerHTML = ""
+  for (let i = (currentPageNumber - 1) * SizeCount; i < currentPageNumber * SizeCount; i++) {
     const cardElement = document.createElement("div");
     const h3 = document.createElement("h3");
     h3.classList.add("title");
     h3.innerText = data[i]["l30"];
     cardElement.append(h3);
-    rootElement.append(cardElement);
+    container.append(cardElement);
+    console.log(h3.innerText)
   }
+  rootElement.append(container)
 }
-
+let data
 const rootElement = document.getElementById("root");
+const container = document.createElement("div")
 const SizeCount = 10;
 let pageCount;
 let currentPageNumber = 1;
@@ -39,31 +43,59 @@ async function getPrice() {
   loadingSpan.innerText = "در حال بارگذاری ...";
   loadingDiv = document.createElement("div");
   loadingDiv.classList.add("loading");
-  rootElement.append(loadingDiv);
+  container.append(loadingDiv);
   try {
     const response = await fetch(url);
     if (!response.ok) {
       loadingSpan.innerText = "  خطای سرور ...";
-      rootElement.append(loadingSpan);
+      container.append(loadingSpan);
     }
-    const data = await response.json();
+    data = await response.json();
     pageCount = Math.floor(data.length / SizeCount);
-    console.log(pageCount);
-    getPageData(data);
+    getPageData();
 
-    rootElement.removeChild(loadingDiv);
+    container.removeChild(loadingDiv);
   } catch (error) {
     loadingSpan.innerText = "خطای سرور   ...";
-    rootElement.append(loadingSpan);
+    container.append(loadingSpan);
   }
 }
 
 getPrice();
 
+
+togglePrevBtn = ()=>{
+    if (currentPageNumber <= 1){
+    currentPageNumber = 1
+    prevButton.disabled = true
+  }
+}
+toggleNextBtn = ()=>{
+    if (currentPageNumber >= pageCount){
+        nextButton.disabled = true
+        currentPageNumber = pageCount
+    }
+}
 const nextButton = document.createElement("button");
 nextButton.innerText = "next";
 nextButton.addEventListener("click", () => {
   currentPageNumber++;
+  toggleNextBtn()
+  if (currentPageNumber > 1)
+    prevButton.disabled = false
+  getPageData()
 
 });
 rootElement.append(nextButton);
+const prevButton = document.createElement("button");
+prevButton.innerText = "prev";
+prevButton.addEventListener("click", () => {
+  currentPageNumber--;
+  if (currentPageNumber < pageCount)
+    nextButton.disabled = false
+  togglePrevBtn(currentPageNumber)
+  getPageData()
+
+});
+rootElement.append(prevButton);
+togglePrevBtn(currentPageNumber)
