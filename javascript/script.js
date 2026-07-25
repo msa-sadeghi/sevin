4dfda0724feef07fda03a0e58bcb2bcf3f17dedd
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-07-25 20:54:50 +0330

    s

--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,28 @@
-// function getPrice() {
-//   console.log("start");
-//   console.log("end");
-// }
-// getPrice();
-
-// function delay() {
-//   return new Promise((resolve) => {
-//     setTimeout(() => resolve("data recieved"), 2000);
-//   });
-// }
-
-// async function getPrice() {
-//   console.log("start");
-//   const result = await delay();
-//   console.log(result);
-//   console.log("end");
-// }
-
-function getPageData(data) {
-  for (let i = 0; i < SizeCount; i++) {
+let data;
+const rootElement = document.getElementById("root");
+const container = document.createElement("div");
+const progressbarinner = document.getElementById("progressbarinner");
+const SizeCount = 10;
+let pageCount;
+let currentPageNumber = 1;
+function getPageData() {
+  container.innerHTML = "";
+  for (
+    let i = (currentPageNumber - 1) * SizeCount;
+    i < currentPageNumber * SizeCount;
+    i++
+  ) {
     const cardElement = document.createElement("div");
     const h3 = document.createElement("h3");
     h3.classList.add("title");
     h3.innerText = data[i]["l30"];
     cardElement.append(h3);
-    rootElement.append(cardElement);
+    container.append(cardElement);
   }
-}
+  progressbarinner.style.width = `${(currentPageNumber / pageCount) * 100}%`;
 
-const rootElement = document.getElementById("root");
-const SizeCount = 10;
-let pageCount;
-let currentPageNumber = 1;
+  rootElement.append(container);
+}
 async function getPrice() {
   const url =
     "https://brsapi.ir/Api/Tsetmc/Sample/Api_FreeBourseWebService.json";
@@ -39,31 +30,58 @@ async function getPrice() {
   loadingSpan.innerText = "در حال بارگذاری ...";
   loadingDiv = document.createElement("div");
   loadingDiv.classList.add("loading");
-  rootElement.append(loadingDiv);
+  container.append(loadingDiv);
   try {
     const response = await fetch(url);
     if (!response.ok) {
       loadingSpan.innerText = "  خطای سرور ...";
-      rootElement.append(loadingSpan);
+      container.append(loadingSpan);
     }
-    const data = await response.json();
+    data = await response.json();
     pageCount = Math.floor(data.length / SizeCount);
-    console.log(pageCount);
-    getPageData(data);
+    getPageData();
 
-    rootElement.removeChild(loadingDiv);
+    container.removeChild(loadingDiv);
   } catch (error) {
     loadingSpan.innerText = "خطای سرور   ...";
-    rootElement.append(loadingSpan);
+    container.append(loadingSpan);
   }
 }
 
 getPrice();
 
+togglePrevBtn = () => {
+  if (currentPageNumber <= 1) {
+    currentPageNumber = 1;
+    prevButton.disabled = true;
+  }
+};
+toggleNextBtn = () => {
+  if (currentPageNumber >= pageCount) {
+    nextButton.disabled = true;
+    currentPageNumber = pageCount;
+  }
+};
+
+const btnContainer = document.createElement("div");
+btnContainer.classList.add('btnContainer')
 const nextButton = document.createElement("button");
 nextButton.innerText = "next";
 nextButton.addEventListener("click", () => {
   currentPageNumber++;
-
+  toggleNextBtn();
+  if (currentPageNumber > 1) prevButton.disabled = false;
+  getPageData();
 });
-rootElement.append(nextButton);
+btnContainer.append(nextButton);
+const prevButton = document.createElement("button");
+prevButton.innerText = "prev";
+prevButton.addEventListener("click", () => {
+  currentPageNumber--;
+  if (currentPageNumber < pageCount) nextButton.disabled = false;
+  togglePrevBtn(currentPageNumber);
+  getPageData();
+});
+btnContainer.append(prevButton);
+rootElement.append(btnContainer);
+togglePrevBtn(currentPageNumber);
